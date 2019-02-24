7ef9791bd537ef43de7c6cf9b7915776e74cb75d
chenbohan <Wanli.Jiang@bmw.com>
2019-02-24 18:58:43 +0800

    refine in 2019.02

--- kalman_filter_1d.py
+++ kalman_filter_1d.py
@@ -1,13 +1,34 @@
 def update(mean1, var1, mean2, var2):
-    new_mean = (var2 * mean1 + var1* mean2)
+    """
+    mean1, var1: prior measurement probability
+    mean2, var2: new measurement probability
+    """
+    new_mean = (var2 * mean1 + var1 * mean2) / (var1 + var2)
     new_var = 1 / (1 / var1 + 1  / var2)
     return [new_mean, new_var]
 
 def predict(mean1, var1, mean2, var2):
-    # mean1: current estimate
-    # var1: current variance
-    # mean2: motion
-    # var2: motion's uncertainty
+    """
+    mean1: current estimate
+    var1: current variance
+    mean2: motion
+    var2: motion's uncertainty
+    """
     new_mean = mean1 + mean2
     new_var = var1 + var2
     return [new_mean, new_var]
+
+# Test data
+measurements = [5.0, 6.0, 7.0, 9.0, 10.0]
+motions = [1.0, 1.0, 2.0, 1.0, 1.0]
+measurement_sig = 4.0
+motion_sig = 2.0
+mu = 0.0 # initial mu
+sig = 1000.0  # initial sig
+
+# KF
+for n in range(len(measurements)):
+    [mu, sig] = update(mu, sig, measurements[n], measurement_sig)
+    print 'update: ', [mu, sig]
+    [mu, sig] = predict(mu, sig, motions[n], motion_sig)
+    print 'predict: ', [mu, sig]
--- kalman_filter_md.py
+++ kalman_filter_md.py
@@ -1,7 +1,11 @@
 import matrix as m
 
+measurements = [1, 2, 3]
+
+x = m.matrix([[0.],[0.]])   # initial state (location & velocity)
+P = m.matrix([[1000., 0.],[0., 1000]]) # covariance matrix (initial uncertainty)
 u = m.matrix([[0.],[0.]])   # external motion
-F = m.matrix([[1., 1.],[0., 1.]])   # next state function
+F = m.matrix([[1., 1.],[0., 1.]])   # transition (next state) function
 H = m.matrix([[1., 0.]])   # measurement function
 R = m.matrix([[1.]])        # measurement uncertainty
 I = m.matrix([[1., 0.],[0., 1.]])   # identity matrix
@@ -22,7 +26,11 @@ def kalman_filter(x, P, measurements):
         x = (F * x) + u
         P = F * P * F.transpose()
 
-        print 'x = ', x
-        print 'P = ', P
+        print 'x = '
+        x.show()
+        print 'P = '
+        P.show()
 
     return x, P
+
+kalman_filter(x, P, measurements)
